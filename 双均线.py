bc463be4874465c404bdfa91e26f786b3346ff08
graceyangfan <yf1825617022@outlook.com>
2021-07-03 14:01:46 +0800

    Update 双均线.py

--- 双均线.py
+++ 双均线.py
@@ -63,6 +63,7 @@ class QuantCenter():
     def get_kline(self, period = PERIOD_M5):
         
         self.Kline = exchange.GetRecords(period)
+
         
         
     def create_order(self, order_type, price, amount):
@@ -88,7 +89,7 @@ class QuantCenter():
 
         return self.exchange.CancelOrder(order_id)
         
-    def refresh_data(self):
+    def refresh_data(self,period = PERIOD_M5):
 
         if not self.get_account():
             return 'false_get_account'
@@ -98,7 +99,7 @@ class QuantCenter():
         if not self.get_depth():
             return 'false_get_depth'
         try:
-            self.get_kline()
+            self.get_kline(period)
         except:
             return 'false_get_K_line_info'
         
@@ -121,21 +122,23 @@ class Strategy():
         
         self.min_buy_money=args.min_buy_money
         self.min_sell_money=args.min_sell_money
+        self.quantcenter.get_kline()
+        self.close_Arr= np.zeros(len(self.quantcenter.Kline))
+        for i,item in enumerate(self.quantcenter.Kline):
+            self.close_Arr[i]=item["Close"]
         
-    def refresh_data(self):
         
-        self.quantcenter.refresh_data()
+        
+    def refresh_data(self,period):
+        
+        self.quantcenter.refresh_data(period)
         self.Amount = self.quantcenter.Amount
         self.Balance = self.quantcenter.Balance
         self.Buy_price = self.quantcenter.Buy
         self.Sell_price = self.quantcenter.Sell
         self.potential_buy_amount = round(self.Balance/self.Sell_price,self.amount_N)
         self.potential_sell_amount = round(self.Amount,self.amount_N)
-    
-        ## use for data_process 
-        self.close_Arr= np.zeros(len(self.quantcenter.Kline))
-        for i,item in enumerate(self.quantcenter.Kline):
-            self.close_Arr[i]=item["Close"]
+        
         
     def make_trade_by_dict(self, trade_dicts):
         for trade in trade_dicts:
@@ -143,6 +146,7 @@ class Strategy():
             trade_amount=round(trade["amount"],self.amount_N)
             trade_id=self.quantcenter.create_order(trade["side"],trade_price,trade_amount)
             self.trade_list.append(trade_id)
+            
     def trade_amout_compute(self, hands_num):
         '''
         hands_num =1 全仓买卖
@@ -159,11 +163,23 @@ class Strategy():
             return True 
     
     def strategy_condition(self,short_period,long_period):
+        
+        ## use for data_process 
+        ## in fmz every time refresh Kline length is 101 
+       # Log("start strategy_condition compute")
+        ## self.quantcenter.Kline is not update  or change smooth 
+       
+        #Log("Kline is update ")
+        self.close_Arr= np.zeros(len(self.quantcenter.Kline))
+        for i,item in enumerate(self.quantcenter.Kline):
+            self.close_Arr[i]=item["Close"]
+            
+        
         fast_arr = TA.SMA(self.close_Arr,short_period)
         slow_arr = TA.SMA(self.close_Arr,long_period)
         
-        cross_over = fast_arr[-1] > slow_arr [-1] and fast_arr[-2] < slow_arr[-2]
-        cross_below = fast_arr [-1] <slow_arr[-1] and fast_arr[-2] > slow_arr[-2]
+        cross_over = fast_arr[-1] > slow_arr[-1]*1.001 and fast_arr[-2] < slow_arr[-2]
+        cross_below = fast_arr[-1] < slow_arr[-1] and fast_arr[-2] > slow_arr[-2]
         trade_side = False
         if cross_over:
             trade_side = "buy"
@@ -175,20 +191,23 @@ class Strategy():
         Asset_state = self.trade_amout_compute(hands_num)
         # 趋势判定是否买卖["buy","sell",False]
         Strategy_state = self.strategy_condition(short_period,long_period)
-        if Asset_state and Strategy_state:
+        if Asset_state:
+            if Strategy_state is False:
+                return False 
+            
             trade_dicts = []
             if Strategy_state == "buy":
                 trade_dicts.append(
                 {
                     "side":"buy",
-                    "price":round(self.Buy_price*0.9999,self.amount_N),
+                    "price":round(self.Buy_price,self.amount_N),
                     "amount":self.min_buy_amount
                 })
             elif Strategy_state == "sell":
                 trade_dicts.append(
                 {
                     "side":"sell",
-                    "price":round(self.Sell_price*1.0001,self.amount_N),
+                    "price":round(self.Sell_price,self.amount_N),
                     "amount":self.min_sell_amount
                 })
             return trade_dicts
@@ -201,8 +220,8 @@ class Args():
     price_N = 4
     min_buy_money =5 
     min_sell_money =5 
-    short_period = 5
-    long_period = 15 
+    short_period = 10
+    long_period = 20
     hands_num = 2
     quantcenter= QuantCenter(exchange) 
     
@@ -211,16 +230,18 @@ def main():
     
     args = Args() 
     strategy = Strategy(args) 
-    strategy.refresh_data()
+    strategy.refresh_data(PERIOD_M5)
     Log("refresh_data ok")
     while(True):
-        Sleep(1000)
+        Sleep(1000*60*5)
+        #time.sleep(60)
         try:
-            strategy.refresh_data()
+            strategy.refresh_data(PERIOD_M5)
             now_trade_dicts = strategy.make_trade_dicts(args.hands_num,args.short_period,args.long_period)
             if now_trade_dicts:
                 strategy.make_trade_by_dict(now_trade_dicts)
                 now_trade_dicts =False
         except:
                 pass 
+        
         
