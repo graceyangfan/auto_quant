ca1adbb3235531ef3276cfe72d7da4e1c4ad4bb1
graceyangfan <yf1825617022@outlook.com>
2022-03-27 10:25:22 +0800

    Update VPVR.py

--- VPVR.py
+++ VPVR.py
@@ -33,7 +33,40 @@ class VPVR:
             last_min = min_idx
             last_max = max_idx
             
-def vpvr(datafeed,value_area_pct):
+            next_min_idx = np.clip(min_idx - 1, 0, len(self.profile) - 1)
+            next_max_idx = np.clip(max_idx + 1, 0, len(self.profile) - 1)
+
+            low_volume = self.profile.iloc[next_min_idx] if next_min_idx != last_min else None
+            high_volume = self.profile.iloc[next_max_idx] if next_max_idx != last_max else None
+
+            if not high_volume or (low_volume and low_volume > high_volume):
+                trial_vol += low_volume
+                min_idx = next_min_idx
+            elif not low_volume or (high_volume and low_volume <= high_volume):
+                trial_vol += high_volume
+                max_idx = next_max_idx
+            else:
+                break
+
+        return self.profile.index[min_idx], self.profile.index[max_idx]
+    
+    def vpvr_indicator(self,df):
+        self.profile = df.groupby("close")["volume"].sum()
+        self.total_volume = self.profile.sum()
+        self.profile_range = self.profile.index.min(), self.profile.index.max()
+        self.poc_idx = self.midmax_idx(self.profile.values.tolist())
+        if self.poc_idx is not None:
+            self.poc_volume = self.profile.iloc[self.poc_idx]
+            self.poc_price = self.profile.index[self.poc_idx]
+            self.value_area = self.calculate_value_area()
+        else:
+            self.poc_volume = None
+            self.poc_price = None
+            self.value_area = [None, None]
+        return self.poc_price,self.value_area[0],self.value_area[1]
+    
+
+    def vpvr(datafeed,value_area_pct):
     '''
     V,C,H,L
     '''
@@ -68,36 +101,4 @@ def vpvr(datafeed,value_area_pct):
             break
     
     return max_vol_POC,datafeed.close[close_sorted_index[min_idx]],datafeed.close[close_sorted_index[max_idx]]
-    
 
-            next_min_idx = np.clip(min_idx - 1, 0, len(self.profile) - 1)
-            next_max_idx = np.clip(max_idx + 1, 0, len(self.profile) - 1)
-
-            low_volume = self.profile.iloc[next_min_idx] if next_min_idx != last_min else None
-            high_volume = self.profile.iloc[next_max_idx] if next_max_idx != last_max else None
-
-            if not high_volume or (low_volume and low_volume > high_volume):
-                trial_vol += low_volume
-                min_idx = next_min_idx
-            elif not low_volume or (high_volume and low_volume <= high_volume):
-                trial_vol += high_volume
-                max_idx = next_max_idx
-            else:
-                break
-
-        return self.profile.index[min_idx], self.profile.index[max_idx]
-    
-    def vpvr_indicator(self,df):
-        self.profile = df.groupby("close")["volume"].sum()
-        self.total_volume = self.profile.sum()
-        self.profile_range = self.profile.index.min(), self.profile.index.max()
-        self.poc_idx = self.midmax_idx(self.profile.values.tolist())
-        if self.poc_idx is not None:
-            self.poc_volume = self.profile.iloc[self.poc_idx]
-            self.poc_price = self.profile.index[self.poc_idx]
-            self.value_area = self.calculate_value_area()
-        else:
-            self.poc_volume = None
-            self.poc_price = None
-            self.value_area = [None, None]
-        return self.poc_price,self.value_area[0],self.value_area[1]
